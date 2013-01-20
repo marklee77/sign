29f29a62e4eab515364e8e2c0c40d025c67d6afa
Mark Stillwell <marklee@fortawesome.org>
2013-01-20 10:53:32 +0000

    okay, not going to work...

--- content/index.php
+++ content/index.php
@@ -2,27 +2,25 @@
 background: white url(img/engcomp.png) no-repeat left top
 ---
 {%- from "message.j2"  import message -%}
-{%- from "schedule.j2" import schedule -%}
 {%- from "image.j2"    import placed_image -%}
 {%- from "rotate.j2"   import rotate -%}
 {%- from "clock.j2"    import clock -%}
 {%- from "weather.j2"  import weather -%}
 
+<!--
 {%- call message(textcolor='red', textsize='36pt', left='50px', top='180px') %}
 Department Pizza Lunch Friday!<br />
-<div style="font-size: 15pt;">Engineering Computing Students &amp; Faculty Invited, Bldg 52 FORUM, 12:30pm</div>
+<div style="font-size: 15pt;"
+>Engineering Computing Students &amp; Faculty Invited, Bldg 52 FORUM, 12:30pm<
+/div>
 {% endcall -%}
 
-{%- call placed_image(image='pizza_slice_256.png', width='200px', left='980px', top='20px') %}
+{%- call placed_image(image='pizza_slice_256.png', width='200px', left='980px', 
+                      top='20px') %}
 {% endcall -%}
+-->
 
-{%- call rotate(name='sched', 
-    pages=[ 'schedule01.php',
-            'schedule02.php',
-            'schedule03.php',
-            'schedule04.php',
-            'schedule05.php'
-    ], seconds=60, 
+{%- call rotate(name='sched', pages=[ 'schedule.php' ], seconds=60, 
                 width='880px', height='820px', left='50px', top='260px') %}
 {% endcall -%}
 
