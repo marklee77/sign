387e0ebde1822ba9751328eddaebba63525b1f0a
Mark Stillwell <marklee@fortawesome.org>
2013-05-24 14:58:38 +0100

    mode...

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
-mkdir -p deploy
+mkdir -m 755 -p deploy
 rsync -pr --delete static/ deploy/
 rsync -pr private/ deploy/
 umask 133
 template-render -d ./templates:./widgets -t index.html.mako -m ./private.yaml \
     -o ./deploy/index.html
-mkdir -p deploy/css
+mkdir -m 755 -p deploy/css
 lessc style/default.less > deploy/css/default.css
 lessc style/main.less > deploy/css/main.css
